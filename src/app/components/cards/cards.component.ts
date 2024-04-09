ac9aaf5140a7dc6f3d0a8716f72a6f234b813b6c
Andres Moncada <desarrollador.sena@asocebu.net>
2024-04-09 12:19:24 -0500

    Se añade la paginación y los estilos

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ExcelService } from '../../services/excel.service';
+import e from 'express';
 
 interface PageEvent {
   first: number;
@@ -20,21 +21,30 @@ export class CardsComponent implements OnInit {
   departamento!: string;
   codigo!: string;
   asociados: any[] = [];
-  asociadosCodigo: any
+  asociadosCodigo: any;
+
+  //Paginador
+  paginatedDataList: any[] = []; // Datos que se muestran en la página actual
+  totalRecords!: number;
+  rows: number = 8; // Número de registros por página
+  first: number = 0; // Índice del primer registro en la página actual
 
   constructor(private route: ActivatedRoute, private router: Router, private excelService: ExcelService) { }
 
   ngOnInit(): void {
     this.departamento = this.route.snapshot.params['departamento'];
-    console.log(this.departamento)
-
+    /* console.log(this.departamento) */
+    
     this.excelService.readExcel().then(
       data => {
-        this.asociados = data.filter(asociados => asociados.departamento1 === this.departamento); 
-        console.log("largo: ", this.asociados.length)
-        console.log("Cards", this.asociados)
+        this.asociados = data.filter(asociados => asociados.departamento1 === this.departamento);
+        this.totalRecords = this.asociados.length; 
+        //Se llama aquí, después de la llamada de los datos, si no, saldrá en blanco
+        this.paginate();
+        /* console.log("largo: ", this.asociados.length)
+        console.log("Cards", this.asociados) */
         this.asociadosCodigo = data.filter(asociados => asociados.codigoAsociado === this.codigo)
-        console.log("Codigo", this.codigo)
+        /* console.log("Codigo", this.codigo) */
       },
       error => {
         console.error('Error al leer el archivo Excel:', error);
@@ -53,4 +63,17 @@ export class CardsComponent implements OnInit {
     }
   }
 
+  onPageChange(event:any) {
+    // Evento contiene la información de la paginación
+    this.first = event.first;
+    this.rows = event.rows;
+    this.paginate();
+  }
+
+  paginate() {
+    // Calcula el índice del último registro en la página actual
+    let lastIndex = this.first + this.rows;
+    // Actualiza paginatedDataList con los datos de la página actual
+    this.paginatedDataList = this.asociados.slice(this.first, lastIndex);
+  }
 }
